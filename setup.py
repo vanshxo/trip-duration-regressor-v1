e0124bda6838965c37a628b0cccef9ef2593b609
vanshxo <khatrivansh43@gmail.com>
2024-08-24 18:46:07 +0530

    folder structure using cookie cutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This Project is based on a machine learning regression system for prediction of trip duration for a taxi in NYC using machine learning techniques and using CI/CD pipelines using best practices and deployment',
+    author='Vansh Khatri',
+    license='MIT',
+)
